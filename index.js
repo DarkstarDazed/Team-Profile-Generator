0c345f6f8e96beee8d4e34089d276f9cc88bc2b9
mediazjr <mediazjr@gmail.com>
2022-01-02 18:10:54 -0800

    required necessary files in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = require('./src/generateHTML');
+
