8539e1f1347ca32bc35d7be8577477b6d1eb9b83
mediazjr <mediazjr@gmail.com>
2022-01-02 19:23:04 -0800

    added newManager function to index.js to ask manager related questions

--- index.js
+++ index.js
@@ -7,3 +7,39 @@ const fs = require('fs');
 
 const generateHTML = require('./src/generateHTML');
 
+const teamArray = [];
+
+function newManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the team managers name?',
+            name: 'name',
+        },
+
+        {
+            type: 'input', 
+            message: 'What is the team managers email?'
+            name: 'email',
+        },
+
+        {
+            type: 'input', 
+            message: 'What is the team managers id?'
+            name: 'id',
+        },
+
+        {
+            type: 'input', 
+            message: 'What is the team managers office number?'
+            name: 'officeNumber',
+        },
+    
+    ]) .then(function(data) {
+        console.log(data)
+        const manager = new Manager(data.name, data.email, data.id, data.officeNumber)
+        console.log(manager)
+        teamArray.push(manager)
+        newPrompt()
+    })
+}
